c8162d0550198964aca6fb565e9bd182f0b09980
amirali <amirali.mnj@gmail.com>
2020-12-13 04:41:38 +0330

    Flock structure implemented.

---
+++ Assets/Scripts/Flock/Flock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Flock : MonoBehaviour
+{
+    public FlockAgent agentPrefab;
+    public FlockBehavior behavior;
+    [Range(2, 1000)] public int flockPopulation;
+    [Range(1f, 100f)] public float speedMultiplier = 10f;
+    [Range(1f, 100f)] public float maxSpeed = 5f;
+    [Range(1f, 10f)] public float neighborRadius = 1.5f;
+    [Range(0f, 1f)] public float avoidanceRadiusMultiplier = 0.5f;
+
+    private const float Density = 0.08f;
+    private List<FlockAgent> _flockAgents = new List<FlockAgent>();
+    private float _sqMaxSpeed;
+    private float _sqNeighborRadius;
+    private float _sqAvoidanceRadius;
+
+    public float SqAvoidanceRadius => _sqAvoidanceRadius;
+
+    
+
+    private void Start()
+    {
+        _sqMaxSpeed = maxSpeed * maxSpeed;
+        _sqNeighborRadius = neighborRadius * neighborRadius;
+        _sqAvoidanceRadius = _sqNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        for (var i = 0; i < flockPopulation; i++)
+            CreateAgent(i);
+    }
+
+    private void CreateAgent(int index)
+    {
+        var agentsPosition = flockPopulation * Density * Random.insideUnitSphere;
+        var agent = Instantiate(agentPrefab, agentsPosition, Random.rotation, transform);
+        agent.name = $"Agent{index}";
+        _flockAgents.Add(agent);
+    }
+}
---
+++ Assets/Scripts/Flock/FlockAgent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(Collider))]
+public class FlockAgent : MonoBehaviour
+{
+    private Flock _agentFlock;
+    public Flock AgentFlock => _agentFlock;
+    
+    private Collider _agentCollider;
+    public Collider AgentCollider => _agentCollider;
+    
+    private void Start()
+    {
+        _agentCollider = GetComponent<Collider>();
+    }
+
+    public void Initialize(Flock flock)
+    {
+        _agentFlock = flock;
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        transform.forward = velocity;
+        transform.position += velocity * Time.deltaTime;
+    }
+}
