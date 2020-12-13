a96c15f00d904d278c9594e2ae87e9bb7fb0b11c
amirali <amirali.mnj@gmail.com>
2020-12-13 12:53:50 +0330

    Squashed 'Behaviors/Flock-Simulation-Unity/' content from commit d31dead
    git-subtree-dir: Behaviors/Flock-Simulation-Unity
    git-subtree-split: d31deaddbb1741cbf24a93cf8da508ff6397c3fd

---
+++ Assets/Scripts/Flock/Behaviors/AlignmentBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignmentBehavior : FilteredFlockBehavior
+{
+    Vector3 currentVelocity;
+    public float smoothTime= 0.2f;
+    public override Vector3 CalculateMove(Flock flock, FlockAgent agent, List<Transform> context)
+    {
+        if (context.Count == 0)
+            return agent.transform.forward;
+        var movement = Vector3.zero;
+        var filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+        if (filteredContext.Count == 0)
+            return agent.transform.forward;
+        filteredContext.ForEach(c => movement += c.transform.forward);
+        movement /= filteredContext.Count;
+        return movement;
+    }
+}
---
+++ Assets/Scripts/Flock/Behaviors/FlockBehavior.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FlockBehavior : ScriptableObject
+{
+    public abstract Vector3 CalculateMove(Flock flock, FlockAgent agent, List<Transform> context);
+}
