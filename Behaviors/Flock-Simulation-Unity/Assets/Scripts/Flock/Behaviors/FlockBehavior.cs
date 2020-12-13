9760a1d34835864adb7d4becf7d582a04a06d477
amirali <amirali.mnj@gmail.com>
2020-12-13 16:27:36 +0330

    Flock deleted.

--- Behaviors/Flock-Simulation-Unity/Assets/Scripts/Flock/Behaviors/FlockBehavior.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FlockBehavior : ScriptableObject
-{
-    public abstract Vector3 CalculateMove(Flock flock, FlockAgent agent, List<Transform> context);
-}
--- Behaviors/Flock-Simulation-Unity/Assets/Scripts/Flock/FilteredBehaviors/ContextFilter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ContextFilter : ScriptableObject
-{
-    public abstract List<Transform> Filter(FlockAgent agent, List<Transform> original);
-}
